f4d100661002b3f50846a568767bd60d0927e790
Caleb <calebdevelops@gmail.com>
2024-03-10 00:12:41 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,12 @@
+import './App.css'
+import Nav from '/src/navbar.jsx'
+import Body from '/src/bodyitems.jsx'
+export default function App() {
+  return (
+    <main>
+      <Nav/>
+      <Body />
+    </main>
+  )
+}
 
