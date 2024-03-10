90dffc415da84fdb55ccc8963b9b7df340db1394
calebdevelops <calebdevelops@gmail.com>
2024-03-10 17:08:36 -0400

    more changes
    more

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,1 +1,9 @@
-
+export default function Nav () {
+    return (
+        <div id='navbar'>
+            <button class="navbutton">Home</button>
+            <button class="navbutton">Who am I?</button>
+            <button class="navbutton">Contact Me</button>
+        </div>
+    )
+}
