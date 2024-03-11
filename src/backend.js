6714457020e18cc8bd021579cd4d19660c601a58
calebdevelops <calebdevelops@gmail.com>
2024-03-11 19:50:16 -0400

    etc.

---
+++ src/backend.js
@@ -0,0 +1,15 @@
+// this is just the backend for this website, just basically a simple page switcher that draws from it.
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname + ".html";
+    //console.log(filename);
+    fs.readFile(filename, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      } 
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+}).listen(3000);
