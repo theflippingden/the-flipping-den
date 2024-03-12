497372fbb6695bf811d05abdc7ac9c9225006823
calebdevelops <calebdevelops@gmail.com>
2024-03-11 22:37:41 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import '/public/style.css'
 import Nav from './components/nav.jsx'
-import Body from './components/body.jsx'
+//import Body from './components/body.jsx'
 export default function App() {
   return (
     <main>
       <Nav/>
-      <Body/>
     </main>
   )
 }
