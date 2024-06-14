67dcb2dd955c80ed2e1d006ea9c7a28ef814e452
calebdevelops <calebdevelops@gmail.com>
2024-06-13 23:08:45 -0400

    pluh
    yuh

--- src/components/nav.jsx
+++
@@ -1,9 +0,0 @@
-export default function Nav () {
-    return (
-        <div id='navbar'>
-            <button class="navbutton">Home</button>
-            <button class="navbutton">Who am I?</button>
-            <button class="navbutton">Contact Me</button>
-        </div>
-    )
-}
