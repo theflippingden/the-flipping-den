fee07440d9c33ffdc58ceff88e8b2648329ea308
calebdevelops <calebdevelops@gmail.com>
2024-07-08 12:19:19 -0500

    bryh

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function mtc() {
   if (document.getElementById("swbtn").textContent="menu") {
     document.getElementById("swbtn").textContent="close";
   }else{
-    document.getElementById("swbtn").textContent="menu"
+    document.getElementById("swbtn").textContent="menu";
   }
 }
 
-document.getElementById("swbtn").addEventListener("click", function() {mtc})
+document.getElementById("swbtn").addEventListener("click", function() {mtc()})
