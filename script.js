8df3e955a0f535ad5e17ed4bd2b21644bd49d7b3
calebdevelops <calebdevelops@gmail.com>
2024-07-10 16:50:12 -0500

    aaA

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-
-$(document).ready(function() {
-$('html, body, *').mousewheel(function(e, delta) {
-this.scrollLeft -= (delta*30);
-e.preventDefault();
-});
-});
+(function() {
+  function scrollHorizontally(e) {
+      e = window.event || e;
+      var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+      this.scrollLeft -= (delta * 40); // Multiplied by 40
+      e.preventDefault();
+  }
+  if (document.body.addEventListener) {
+      // IE9, Chrome, Safari, Opera
+      document.body.addEventListener('mousewheel', scrollHorizontally, false);
+      // Firefox
+      document.body.addEventListener('DOMMouseScroll', scrollHorizontally, false);
+  } else {
+      // IE 6/7/8
+      document.body.attachEvent('onmousewheel', scrollHorizontally);
+  }
+})();
