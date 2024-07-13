24389a055458506f44cd922d42fdffa79c98e8f5
calebdevelops <calebdevelops@gmail.com>
2024-07-13 15:30:44 -0500

    hm

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-(function() {
-  function scrollHorizontally(e) {
-      e = window.event || e;
-      var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-      this.scrollLeft -= (delta * 40); // Multiplied by 40
-      e.preventDefault();
-  }
-  if (document.body.addEventListener) {
-      // IE9, Chrome, Safari, Opera
-      document.body.addEventListener('mousewheel', scrollHorizontally, false);
-      // Firefox
-      document.body.addEventListener('DOMMouseScroll', scrollHorizontally, false);
-  } else {
-      // IE 6/7/8
-      document.body.attachEvent('onmousewheel', scrollHorizontally);
-  }
-})();
+$(function() {
+    $("html, body").mousewheel(function(event, delta) {
+       this.scrollLeft -= (delta * 30);
+       event.preventDefault();
+    });
+ });
