fe7216b3a0505185a2703885beed1ff6480e5e1b
cdev <calebdevelops@gmail.com>
2024-12-17 19:54:36 -0500

    restart

--- script.js
+++
@@ -1,7 +0,0 @@
-var outtxt = document.getElementById('outtext');
-function openMenu() {
-  outtxt.style.width = '20vw';
-  outtxt.style.height = '20vw';
-  outtxt.style.fontSize = '4vw';
-}
-outtxt.addEventListener("click", openMenu);
