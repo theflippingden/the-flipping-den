4e48828164bdff60f2a5d40aa9572f8cc02dd5c7
cdev <calebdevelops@gmail.com>
2024-12-18 21:59:43 -0500

    Color.
    Color.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var storageCircle = document.getElementById('storage');
-function greenOut(){
-    storageCircle.style.backgroundColor = 'rgb(100,200,100)';
-    storageCircle.style.opacity = '0%';
-    document.body.style.backgroundColor = 'rgb(100,200,100)';
+var themea = document.getElementById("theme");
+var themeb = document.getElementById("theme2");
+var themec = document.getElementById("theme3");
+var themed = document.getElementById("theme4");
+var themee = document.getElementById("theme5");
+var themef = document.getElementById("theme6");
+function openMenu() {
+    themea.style.top = '10vh';
+    themeb.style.top = '20vh';
+    themec.style.top = '30vh';
+    themed.style.top = '40vh';
+    themee.style.top = '50vh';
+    themef.style.top = '60vh';
 }
-storageCircle.addEventListener('click', greenOut);
-function shake() {
-    document.getElementById('coax').style.transform = 'rotate(' + Math.random()*30 + 'deg)';
-    console.log('skibidi');
-}
-document.getElementById('coax').addEventListener('click', shake);
+
+themea.addEventListener('click', openMenu);
