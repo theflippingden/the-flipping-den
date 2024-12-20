381c1f12af0945384c121f79bd8489d013573d75
cdev <calebdevelops@gmail.com>
2024-12-19 22:33:47 -0600

    Change.
    Change.

--- index.js
+++ index.js
@@ -4,13 +4,3 @@ var themec = document.getElementById("theme3");
 var themed = document.getElementById("theme4");
 var themee = document.getElementById("theme5");
 var themef = document.getElementById("theme6");
-function openMenu() {
-    themea.style.top = '10vh';
-    themeb.style.top = '20vh';
-    themec.style.top = '30vh';
-    themed.style.top = '40vh';
-    themee.style.top = '50vh';
-    themef.style.top = '60vh';
-}
-
-themea.addEventListener('click', openMenu);
