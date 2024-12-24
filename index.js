7cc4004bfb52650e590a6d4cd85aacd824bae1a9
cdev <calebdevelops@gmail.com>
2024-12-23 20:02:06 -0600

    brrr

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ var themed = document.getElementById("theme4");
 var themee = document.getElementById("theme5");
 var themef = document.getElementById("theme6");
 var themes = document.getElementsByClassName("themes")
-function changeColor(clr) {
-    document.getElementById("title").style.color = clr;
-}
 function showCS () {
     document.getElementById("themecell").style.visibility = "visible";
     document.getElementById("themecellarrow").style.visibility = "visible";
@@ -16,11 +13,15 @@ function hideCS () {
     document.getElementById("themecell").style.visibility = "hidden";
     document.getElementById("themecellarrow").style.visibility = "hidden";
 }
+function changeColor(clr) {
+    document.getElementById("title").style.color = clr;
+    hideCS();
+}
+
 themea.addEventListener("click", function(){changeColor("var(--green)")});
 themeb.addEventListener("click", function(){changeColor("var(--red)")});
 themec.addEventListener("click", function(){changeColor("var(--blue)")});
 themed.addEventListener("click", function(){changeColor("var(--yellow)")});
 themee.addEventListener("click", function(){changeColor("var(--pink)")});
 themef.addEventListener("click", function(){changeColor("var(--aqua)")});
-document.getElementById("themeall").addEventListener("mouseover", showCS);
-document.getElementById("themecellcollider").addEventListener("mouseout", hideCS);
+document.getElementById("themeall").addEventListener("click", showCS);
