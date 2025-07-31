d056948d6903e9254d583108736df7378dc99ef7
theflippingden <theflippingden@gmail.com>
2025-07-31 14:12:23 -0500

    Menu (unfinished)
    still need to add extra computer functionality but its fine on mobile i do need to still fix the scrollbug tho

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//let's see if this is the issue!
+var menuBtn = document.getElementById("menu-button");
+var menuOpen = false;
+function toggleMenu() {
+    if (menuOpen == false){
+        document.getElementById("header").style.height = "90vh";
+        document.getElementById("header").style.alignItems = "top";
+        document.getElementById("header-title").style.display = "none";
+        document.getElementById("opacity-container").style.opacity = "50%";
+        menuBtn.innerHTML = "<i class='material-symbols-outlined'>close</i>";
+        document.getElementById("open-menu").style.display = "flex";
+        menuOpen = true;
+    } else {
+        document.getElementById("header").style.height = "10vh";
+        menuBtn.innerHTML = "<i class='material-symbols-outlined'>menu</i>";
+        document.getElementById("opacity-container").style.opacity = "100%";
+        document.getElementById("open-menu").style.display = "none";
+        document.getElementById("header-title").style.display = "inline";
+        menuOpen = false;
+    }
+    
+};
+menuBtn.addEventListener("click", toggleMenu);
