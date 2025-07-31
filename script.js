e9b94737710619d99ee1acefe2cc60114447f75f
theflippingden <theflippingden@gmail.com>
2025-07-31 12:16:11 -0500

    fixes

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function checkHeader() {
-    console.log("CHECKING!");
-    if (window.scrollY > 50) {
-        document.getElementById("header").style.opacity = "50%";
-    }
-};
-window.addEventListener("scroll", checkHeader);
-
+//let's see if this is the issue!
