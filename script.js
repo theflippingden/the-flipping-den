55ccdb04a892c66f302ee8ee7d7cdf728c237a38
theflippingden <theflippingden@gmail.com>
2025-08-01 18:07:18 -0500

    night mode works on mobile now
    Now i gotta fix the blue button bug on iphone

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 //fix for the waves so they stretch, need js math for this 1
 var waveJS = document.getElementsByClassName("waves");
+var menuBtn = document.getElementById("menu-button");
+var menuOpen = false;
+var menuSelects = document.getElementsByClassName("menu-selects");
+var computer = window.matchMedia("(min-width: 768px)");
+var darkMode = false;
+var nightBtn = document.getElementById("night-button");
+var rootJS = document.documentElement;
+var headerTitle = document.getElementById("header-title");
+var headerz = document.getElementById("header");
 for (var i=0; i<waveJS.length; i++){
     document.getElementsByClassName("waves")[i].style.transform = "scaleX(calc(" + window.innerWidth + "/380))";
 }
+
 function resizeWaves() {
     for (var i=0; i<waveJS.length; i++){
         document.getElementsByClassName("waves")[i].style.transform = "scaleX(calc(" + window.innerWidth + "/380))";
     }
 }
-var menuBtn = document.getElementById("menu-button");
-var menuOpen = false;
-var menuSelects = document.getElementsByClassName("menu-selects");
+
 function toggleMenu() {
     if (menuOpen == false){
         document.getElementById("opacity-container").style.opacity = "50%";
         menuBtn.innerHTML = "<i class='material-symbols-outlined'>close</i>";
         document.getElementById("open-menu").style.left = "2.5vw";
         document.getElementById("funtext").style.transform = "skew(-3deg, -3deg)";
+        if (!computer.matches) {
+            nightBtn.style.display = "inline-block";
+            headerTitle.style.display = "none";
+            headerz.style.justifyContent = "space-around";
+            nightBtn.style.marginLeft = "0";
+            menuBtn.style.width = "20vh";
+            nightBtn.style.width = "20vh";
+        }
         menuOpen = true;
     } else {
         menuBtn.innerHTML = "<i class='material-symbols-outlined'>menu</i>";
@@ -24,12 +40,18 @@ function toggleMenu() {
         document.getElementById("open-menu").style.left = "102.5vw";
         document.getElementById("funtext").style.transform = "skew(3deg, 3deg)";
         menuOpen = false;
+        if (!computer.matches) {
+            nightBtn.style.display = "none";
+            headerTitle.style.display = "inline-block";
+            headerz.style.justifyContent = "space-between";
+            nightBtn.style.marginLeft = "auto";
+            menuBtn.style.width = "10vh";
+            nightBtn.style.width = "10vh";
+        }
     }
     
 };
-var darkMode = false;
-var nightBtn = document.getElementById("night-button");
-var rootJS = document.documentElement;
+
 function toggleDarkMode() {
     if (darkMode == false) {
         rootJS.style.setProperty('--white', '#1c1c1c');
